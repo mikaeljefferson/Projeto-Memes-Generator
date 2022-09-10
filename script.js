62f9363af1f05d9b8e60976b9ccb470e02a5ef57
mikael jefferson da silva <mikaeljeffeson@gmail.com>
2022-09-10 10:25:57 -0300

    testeteste

---
+++ script.js
@@ -0,0 +1,11 @@
+window.onload = function (){
+    const inputText = document.getElementById('text-input');
+    const memeText = document.getElementById('meme-text');
+    const memeImage = document.getElementById('meme-image');
+    const inputImg = document.getElementById('meme-insert');
+
+    function 
+
+
+
+}
